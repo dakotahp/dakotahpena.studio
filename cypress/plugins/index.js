fa92d917f6c86f45388dcf14d8ed7684b12001e1
DP <git@dakotahpena.dev>
2021-07-23 21:00:17 -0700

    Lock down viewport

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,20 +22,31 @@ module.exports = (on, config) => {
 
   const {viewportWidth: w, viewportHeight: h} = config
   on('before:browser:launch', (browser = {}, launchOptions) => {
-    switch (browser.name) {
-      //browser.family === 'chromium' && browser.name !== 'electron')
-      case 'chrome':
-        launchOptions.args.push(`--window-size=${w},${h}`)
-        /*
-        launchOptions.push('--cast-initial-screen-width=1600')
-        launchOptions.push('--cast-initial-screen-height=900')
-          */
-        break
-      case 'electron':
-        launchOptions.preferences.width = w;
-        launchOptions.preferences.height = h;
-        break
+    if (browser.name === 'chrome') {
+      // fullPage screenshot size is 1400x1200 on non-retina screens
+      // and 2800x2400 on retina screens
+      launchOptions.args.push('--window-size=1400,1200')
+
+      // force screen to be non-retina (1400x1200 size)
+      launchOptions.args.push('--force-device-scale-factor=1')
+
+      // force screen to be retina (2800x2400 size)
+      // launchOptions.args.push('--force-device-scale-factor=2')
     }
+
+    if (browser.name === 'electron') {
+      // fullPage screenshot size is 1400x1200
+      launchOptions.preferences.width = 1400
+      launchOptions.preferences.height = 1200
+    }
+
+    if (browser.name === 'firefox') {
+      // menubars take up height on the screen
+      // so fullPage screenshot size is 1400x1126
+      launchOptions.args.push('--width=1400')
+      launchOptions.args.push('--height=1200')
+    }
+
     return launchOptions
   })
 
